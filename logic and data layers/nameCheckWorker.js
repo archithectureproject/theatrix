433910ea67f016c0e7cde54e9de43841bd271a33
GiladBoudman <budithebudman@gmail.com>
2024-03-24 00:04:53 +0200

    add documentation
    Co-Authored-By: ishay <141156460+idshay16@users.noreply.github.com>

--- logic and data layers/nameCheckWorker.js
+++ logic and data layers/nameCheckWorker.js
@@ -1,10 +1,12 @@
 // nameCheckWorker.js
-
-self.onmessage = function(e) {
-    var firstName = e.data.firstName;
-    var lastName = e.data.lastName;
-    var name_pattern = /[^a-zA-Z\u0590-\u05fe\s]/;
-    var resultFirstName = name_pattern.test(firstName) || firstName == '';
-    var resultLastName = name_pattern.test(lastName) || lastName == '';
-    postMessage(resultFirstName || resultLastName);
+/**
+ * This is a web worker and when its called, it checks wether the first and last names are ilegal according to our pattern
+ */
+self.onmessage = function(dic) {
+    var firstName = dic.data.firstName;
+    var lastName = dic.data.lastName;
+    var name_pattern = /[^a-zA-Z\u0590-\u05fe\s]/; //creates a ilegal pattern
+    var resultFirstName = name_pattern.test(firstName) || firstName == ''; //checks if the first name is not valid or empty
+    var resultLastName = name_pattern.test(lastName) || lastName == ''; //checks if the last name is not valid or empty
+    postMessage(resultFirstName || resultLastName); // returns true if one or more of the names is not valid
 };
